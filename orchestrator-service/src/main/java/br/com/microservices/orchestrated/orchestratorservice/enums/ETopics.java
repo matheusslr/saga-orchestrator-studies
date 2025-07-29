d79d9a60ae101f73b9184b44d0c4cf867e36c036
Matheus Santos <matheusslr@hotmail.com>
2025-07-29 17:49:59 -0300

    build: connecting services with kafka

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/enums/ETopics.java
@@ -0,0 +1,21 @@
+package br.com.microservices.orchestrated.orchestratorservice.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ETopics {
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    NOTIFY_ENDING("notify-ending"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail");
+    private String topic;
+}
