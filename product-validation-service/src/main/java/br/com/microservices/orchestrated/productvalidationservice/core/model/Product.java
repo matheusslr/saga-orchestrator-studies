bcd58ddc0a22d885787be1911672b4ce1112788d
Matheus Santos <matheusslr@hotmail.com>
2025-08-04 08:33:29 -0300

    feat: product and validation entities

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Product.java
@@ -0,0 +1,23 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String code;
+}
+
---
+++ product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.product (id, code) VALUES (1, 'COMIC_BOOKS');
+INSERT INTO public.product (id, code) VALUES (2, 'BOOKS');
+INSERT INTO public.product (id, code) VALUES (3, 'GAMES');
+INSERT INTO public.product (id, code) VALUES (4, 'ELECTRONICS');
